8fdfe4ac94a5b18207e62e6839954ad5e5126b19
Nathan Budiman <jonathan@radialogica.com>
2020-06-01 15:14:59 -0500

    Initial Test

---
+++ GitVal (Github) program/GitVal (Github) program/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitVal__Github__program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
